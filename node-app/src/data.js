8c4fd5eecae6ab9609be389b8a25b5320570b1f9
ibado <ignaciobado.unc@gmail.com>
2020-12-28 12:03:32 -0300

    Make SQL queries parametrized

--- node-app/src/data.js
+++ node-app/src/data.js
@@ -24,25 +24,25 @@ class RoutineRepository {
     }
 
     async findById(id) {
-        const query = `SELECT * FROM routines WHERE id = ${id};`;
-        const result = await this.dbConnection.query(query);
+        const query = 'SELECT * FROM routines WHERE id = ?;';
+        const result = await this.dbConnection.query(query, [id]);
         return result.length > 0 ? result[0] : undefined;
     }
 
     async add(routine) {
-        const query = `INSERT INTO routines (name) VALUES ("${routine.name}");`;
-        const result = await this.dbConnection.query(query);
+        const query = 'INSERT INTO routines (name) VALUES (?);';
+        const result = await this.dbConnection.query(query, [routine.name]);
         return { id: result.insertId, name: routine.name};
     }
 
     async remove(id) {
-        return await this.dbConnection.query(`DELETE FROM routines WHERE id=${id};`);
+        return await this.dbConnection.query('DELETE FROM routines WHERE id = ?;', [id]);
     }
 
     async update(routine) {
         const { id, name } = routine;
-        const query = `UPDATE routines SET name=\"${name}\" WHERE id = ${id}`;
-        return await this.dbConnection.query(query);
+        const query = 'UPDATE routines SET name = ? WHERE id = ?';
+        return await this.dbConnection.query(query, [name, id]);
     }
 }
 
