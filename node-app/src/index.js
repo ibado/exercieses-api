406203d8fade97842e732ac3542d8ab4adedee94
ibado <ignaciobado.unc@gmail.com>
2020-12-29 12:49:33 -0300

    Add router

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -3,66 +3,19 @@ const Joi = require('joi');
 const RoutineRepository = require('./data');
 
 const app = express();
+const router = express.Router();
 const repository = new RoutineRepository();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello from express sarasa');
-});
-
-app.get('/api/routines', (req, res) => {
-    console.log("getting routines...");
-
-    repository.getAll()
-        .then(value => res.send(value))
-        .catch(error => res.status(500).send(`shit happened: ${error}`));
-});
-
-app.get('/api/routines/:id', (req, res) => {
-    const id = Number(req.params.id);
-    if (!id) {
-        res.status(400).send(`${req.params.id} is an invalid id`);
-        return;
-    }
-    repository.findById(id).then(value => {
-        if (!value) {
-            res.status(404).send("The routine with the given ID does not exist");
-            return;
-        }
-        res.send(value);
-    });
-});
-
-app.post('/api/routines', (req, res) => {
-
-    const { error } = validateRoutine(req.body);
-
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-
-    const routine = {
-        name: req.body.name,
-    };
-    repository.add(routine).then((value) => {
-        res.send(value);
-    });
-});
-
-app.put('/api/routines/:id', (req, res) => {
-    const id = Number(req.params.id);
-    if (!id) {
-        res.status(400).send(`${req.params.id} is an invalid id`);
-        return;
-    }
-
-    repository.findById(id).then(value => {
-        if (!value) {
-            res.status(404).send("The routine with the given ID does not exist");
-            return;
-        }
+router.route('/routines')
+    .get((req, res) => {
+        console.log("getting routines...");
+        repository.getAll()
+            .then(value => res.send(value))
+            .catch(error => res.status(500).send(`shit happened: ${error}`));
+    })
+    .post((req, res) => {
 
         const { error } = validateRoutine(req.body);
 
@@ -71,24 +24,70 @@ app.put('/api/routines/:id', (req, res) => {
             return;
         }
 
-        value.name = req.body.name;
-        repository.update(value).then(v => res.send(value));
-    }); 
-});
+        const routine = { name: req.body.name, };
 
-app.delete('/api/routines/:id', (req, res) => {
-    const id = req.params.id;
+        repository.add(routine).then((value) => res.send(value));
 
-    repository.remove(id).then((value) => {
-        const result = value.affectedRows;
-        if (result == 1) {
-            res.send("Routine deleted");
-        } else {
-            res.status(404).send("The routine with the given ID does not exist");
-        }
     });
 
+router.route('/routines/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        if (!id) {
+            res.status(400).send(`${req.params.id} is an invalid id`);
+            return;
+        }
+        repository.findById(id).then(value => {
+            if (!value) {
+                res.status(404).send("The routine with the given ID does not exist");
+                return;
+            }
+            res.send(value);
+        });
+    })
+    .put((req, res) => {
+
+        const id = Number(req.params.id);
+        if (!id) {
+            res.status(400).send(`${req.params.id} is an invalid id`);
+            return;
+        }
+
+        repository.findById(id).then(value => {
+            if (!value) {
+                res.status(404).send("The routine with the given ID does not exist");
+                return;
+            }
+
+            const { error } = validateRoutine(req.body);
+
+            if (error) {
+                res.status(400).send(error.details[0].message);
+                return;
+            }
+
+            value.name = req.body.name;
+            repository.update(value).then(v => res.send(value));
+        });
+    })
+    .delete((req, res) => {
+        const id = req.params.id;
+
+        repository.remove(id).then((value) => {
+            const result = value.affectedRows;
+            if (result == 1) {
+                res.send("Routine deleted");
+            } else {
+                res.status(404).send("The routine with the given ID does not exist");
+            }
+        });
+    });
+
+app.get('/', (req, res) => {
+    res.send('Hello from express sarasa');
 });
+
+app.use('/api', router);
 
 function validateRoutine(routine) {
     return Joi.object({
